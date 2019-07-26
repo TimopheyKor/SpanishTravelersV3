a63813f86ec2a80ff63291d01d10880341834877
Tim Korolev <timopheykor@gmail.com>
2019-07-26 09:42:47 -0500

    Base files
    Some code outline, not much code, mainly a template at this point. Organized better than in previous iterations such as V1 and V2 for (hopefully) better results.

---
+++ TheSpanishTravelersProjectJS.js
@@ -0,0 +1,7 @@
+/* For an overview of what this JS is for, see top comment of TheSpanishTravelersProjectV3.html
+Author: Timophey Korolev
+Email: timophey.korolev@marquette.edu
+Libraries: jQuery, Bootstrap, Leaflet, Esri
+The structure of this JS is learned from an example of a similar project, the Boyd Map.
+*/
+
