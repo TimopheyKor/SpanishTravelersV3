587207b2fc31f0e264bfc1c291a60ed311a46ca4
Tim Korolev <timopheykor@gmail.com>
2020-07-29 13:40:28 -0500

    Updated JSON generator with "relations" reading

--- geoPointsJsonGenerator.py
+++ geoPointsJsonGenerator.py
@@ -29,23 +29,26 @@ for row in sheet.iter_rows(max_row=13, max_col=12, min_col=2):
             if imgLink == "None":
                 imgLink = "No_image_available.svg"
             print(cellName + " : Link : " + imgLink, end="\n")
-        if ".D" in cellName: # Column B contains the captions / labels for the images (tombstone)
+        if ".D" in cellName: # Column D contains the captions / labels for the images (tombstone)
             tombstone = str(cell.value)
             print(cellName + " : Tombstone : " + tombstone, end="\n")
-        if ".G" in cellName: # Column D contains the latitude of the feature
+        if ".G" in cellName: # Column G contains the latitude of the feature
             latitude = cell.value # Should be kept as a double
             print(cellName + " : Latitude : " + str(latitude))
-        if ".H" in cellName: # Column D contains the longitude of the feature
+        if ".H" in cellName: # Column H contains the longitude of the feature
             longitude = cell.value # Should be kept as a double
             print(cellName + " : Latitude : " + str(longitude))
         if ".L" in cellName: # Column L contains the name of the feature
             imgName = str(cell.value)
             print(cellName + " : Name : " + imgName)
+        if ".J" in cellName: # Column J contains the relations to the feature
+            relations = str(cell.value)
+            print(cellName + " : Relations : " + relations)
     print("Opening JSON file... \n")
     # Reading through the current JSON file
     with open ("testPoints.json") as json_file:
         data = json.load(json_file)
         temp = data["features"]
-        y = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [latitude, longitude]}, "properties": {"imgURL": imgLink, "name": imgName, "tombstone": tombstone}}
+        y = {"type": "Feature", "geometry": {"type": "Point", "coordinates": [latitude, longitude]}, "properties": {"imgURL": imgLink, "name": imgName, "tombstone": tombstone, "relations": relations}}
         temp.append(y)
     write_json(data)
