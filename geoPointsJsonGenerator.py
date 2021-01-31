e02b9f52ef1aaff8fcf423759c2b77424eceb2e2
Tim Korolev <timopheykor@gmail.com>
2021-01-31 17:27:58 -0600

    Updated JSON points
    Updated JSON format to match MAM's data, updated Python automation to match, updated data in JSON file.

--- geoPointsJsonGenerator.py
+++ geoPointsJsonGenerator.py
@@ -8,8 +8,8 @@ from pathlib import Path
 I_TOMBSTONE = 0
 I_DESCRIPTION = 1
 I_IMAGE_URL = 2
-MINIMUM_RELATIONS = 3
-CELLNAME_ID_POS = slice(17, 19)
+MINIMUM_RELATIONS = 2
+CELLNAME_ID_POS = slice(14, 16)
 
 # Setting the file path to the .xlsx checklist file...
 checklistFile = Path("Map_Points_Spreadsheet.xlsx")
@@ -27,7 +27,7 @@ data["features"] = []              # Empty Feature List
 # def processCell(cellID):
 
 # Helper function to take in one row of data from the .xlsx file and output a JSON feature.
-def appendData(longitude, latitude, imageArray):
+def appendData(longitude, latitude, imageArray, galleryName):
     return {
         "type": "Feature",
         "geometry": {
@@ -38,54 +38,73 @@ def appendData(longitude, latitude, imageArray):
             ]
         },
         "properties": {
+            "name": galleryName,
             "images": imageArray
         }
     }
 
 # Iterating through the sheet to get URL / image location values
-for row in sheet.iter_rows(max_row=13, max_col=15, min_row=2, min_col=2):
+for row in sheet.iter_rows(max_row=15, max_col=20, min_row=2, min_col=1):
     # Temporary variables to hold and create image objects.
     tempImageArray = []
     tempImageObject = {}
+    latitude, longitude = 0, 0
+    galleryName = ""
     for cell in row:
         # Type casting the cell name to a string.
         # Narrow the string down to the characters used to identify the cell.
         cellName = str(cell)
         cellName = cellName[CELLNAME_ID_POS]
-        # Column B contains the URL of the main image
-        if ".B" in cellName:
+        print(cellName)
+        # Column E contains the name of the gallery
+        if ".E" in cellName: 
+            galleryName = str(cell.value)
+            print(galleryName)
+        # Column A contains the tombstone for the main image
+        if ".A" in cellName:
+            tombstone = str(cell.value)
+            tempImageObject["tombstone"] = tombstone
+            print(tempImageObject)
+        # Column C contains the tombstone of the main image
+        if ".C" in cellName: 
+            description = str(cell.value)
+            tempImageObject["description"] = description
+            print(tempImageObject)
+        # Column D contains the URL of the main image
+        if ".D" in cellName:
             imgLink = str(cell.value)
             if imgLink == "None":
                 imgLink = "No_image_available.svg"
             tempImageObject["imgURL"] = imgLink
-        # Column C contains the tombstone for the main image
-        if ".C" in cellName:
-            tombstone = str(cell.value)
-            tempImageObject["tombstone"] = tombstone
+            print(tempImageObject)
             tempImageArray.append(tempImageObject)
-            tempImageObject = {} # Clear temp object after creating main image
-        # Column F contains the latitude of the feature
-        if ".F" in cellName:
-            latitude = cell.value
-        # Column G contains the longitude of the feature
+        # Column G contains the latitude of the feature
         if ".G" in cellName:
+            latitude = cell.value
+        # Column H contains the longitude of the feature
+        if ".H" in cellName:
             longitude = cell.value
-        # Columns containing related images
-        if cellName in ".I.J.K.L.M.N.O":
+        # Columns containing links of related images
+        if cellName in ".K.N.Q.T.W":
+            imgLink = str(cell.value)
+            if imgLink == "None":
+                imgLink = "No_image_available.svg"
+            tempImageObject["imgURL"] = imgLink
+        # Columns containing text of related images
+        if cellName in ".L.O.R.U.X":
             relation = str(cell.value)
             # Split the relation image data by the separating |'s used in the .xlsx file
             relationArray = relation.split("|") 
             # To prevent OOB errors:
             # This is to ensure that at least the three required fields are in the array.
             if len(relationArray) >= MINIMUM_RELATIONS:
-                tempImageObject["imgURL"]      = relationArray[I_IMAGE_URL]
                 tempImageObject["tombstone"]   = relationArray[I_TOMBSTONE]
                 tempImageObject["description"] = relationArray[I_DESCRIPTION]
                 tempImageArray.append(tempImageObject)
             tempImageObject = {} # Clear temp object after creating related image
     
     # Appending the data found in this row to the data dict...
-    obj = appendData(longitude, latitude, tempImageArray)
+    obj = appendData(longitude, latitude, tempImageArray, galleryName)
     data["features"].append(obj)
 
 # Writing the collected data to a json file
